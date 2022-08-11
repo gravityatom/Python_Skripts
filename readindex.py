eee548cb005efee2cf13fe335d8cfec2d11cf793
gravityatom <ramirezatom@gmail.com>
2022-08-10 22:28:40 -0700

    initial working version of readindex.py

---
+++ readindex.py
@@ -0,0 +1,17 @@
+#!usr/bin/python3
+
+import argparse
+import sys
+import re
+import os.path
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file', type = str, required = True)
+    parser.add_argument('--index', type = str, required = True, \
+    default = "autoread")
+    args = parser.parse_args()
+    if (os.poath.exists(args.file)) is False:
+        print (f"ERROR: File ({args.file}) does not exist!")
+        exit()
+
